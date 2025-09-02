07fc22bf366fabea9ab064544366d05b1ffca932
henriquemota <mota.henrique@gmail.com>
2025-09-01 21:32:17 -0300

    jokenpo iniciado

--- sources/hello/App.js
+++ sources/hello/App.js
@@ -1,19 +1,42 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Image, Alert } from 'react-native'
+
+// importacao das imagens
+import pedra from './assets/pedra.png'
+import papel from './assets/papel.png'
+import tesoura from './assets/tesoura.png'
 
 export default function App() {
+	function handlePress(jogada) {
+		Alert.alert('Jogada', `Você escolheu ${jogada}`)
+	}
+
 	return (
 		<View style={styles.container}>
-			<View style={[styles.quadrado, { backgroundColor: 'blue' }]}></View>
-			<View style={[styles.quadrado, { backgroundColor: 'green' }]}></View>
-			<TouchableOpacity style={[styles.quadrado, { backgroundColor: 'green' }]}></TouchableOpacity>
+			<Text style={{ fontSize: 24, fontWeight: 'bold' }}>Pedra, Papel e Tesoura</Text>
+			<Text style={{ fontSize: 18 }}>Escolha sua jogada</Text>
+
+			<View style={{ flexDirection: 'row', gap: 16 }}>
+				<TouchableOpacity onPress={() => handlePress('pedra')}>
+					<Image source={pedra} style={styles.imagem} />
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => handlePress('papel')}>
+					<Image source={papel} style={styles.imagem} />
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => handlePress('tesoura')}>
+					<Image source={tesoura} style={styles.imagem} />
+				</TouchableOpacity>
+			</View>
 			<StatusBar style='auto' />
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
+	imagem: {
+		height: 120,
+		width: 120,
+	},
 	quadrado: {
 		height: 100,
 		width: 100,
@@ -21,6 +44,9 @@ const styles = StyleSheet.create({
 	},
 	container: {
 		flex: 1,
-		backgroundColor: '#ff0000',
+		gap: 16,
+		backgroundColor: '#fff',
+		justifyContent: 'center',
+		alignItems: 'center',
 	},
 })
