5b1accdc6d58ba157470a7864ed61af0cc7a9b92
henriquemota <mota.henrique@gmail.com>
2025-10-13 09:19:19 -0300

    atualizando o codigo no github

--- sources/firebase/src/views/enderecos.js
+++ sources/firebase/src/views/enderecos.js
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import { View, Button, Alert, ActivityIndicator } from 'react-native'
-import { TextInput } from 'react-native-paper'
+import { useState, useEffect } from 'react'
+import { View, Button, Alert, ActivityIndicator, FlatList } from 'react-native'
+import { TextInput, Text } from 'react-native-paper'
 import axios from 'axios'
 import { db } from '../services/_firebase'
-import { ref, set } from 'firebase/database'
+import { ref, set, get, child } from 'firebase/database'
 
 const Enderecos = () => {
 	const baseUrl = 'https://dispositivosmoveis2025-default-rtdb.firebaseio.com/enderecos.json'
 	// estados dos campos
+	const [data, setData] = useState([])
 	const [loading, setLoading] = useState(false)
 	const [rua, setRua] = useState('')
 	const [numero, setNumero] = useState('')
@@ -60,6 +61,26 @@ const Enderecos = () => {
 			.finally(() => setLoading(false))
 	}
 
+	const _readData = () => {
+		const dbRef = ref(db)
+		get(child(dbRef, 'enderecos/'))
+			.then((snapshot) => {
+				if (snapshot.exists()) {
+					const val = snapshot.val()
+					setData(Object.entries(val).map(([id, item]) => ({ id, ...item })))
+				} else {
+					setData([])
+				}
+			})
+			.catch((error) => {
+				console.error(error)
+			})
+	}
+
+	useEffect(() => {
+		_readData()
+	}, [, _handleSaveFb])
+
 	return (
 		<View style={{ gap: 16 }}>
 			<TextInput label='Rua' value={rua} onChangeText={setRua} />
@@ -70,6 +91,20 @@ const Enderecos = () => {
 
 			<Button onPress={_handleSaveFb} title='Salvar' />
 			<ActivityIndicator animating={loading} size='large' />
+			<FlatList
+				data={data}
+				renderItem={({ item }) => (
+					<View style={{ borderWidth: 1, margin: 4, borderRadius: 4, gap: 2, padding: 8 }}>
+						<Text>
+							{item.rua}, {item.numero} - {item.bairro}
+						</Text>
+						<Text>
+							{item.cidade} - {item.estado}
+						</Text>
+					</View>
+				)}
+				keyExtractor={(item) => item.id}
+			/>
 		</View>
 	)
 }
