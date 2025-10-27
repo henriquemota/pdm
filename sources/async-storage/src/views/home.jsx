965bd452a014dbbdfb6db260dc4b1774ce214bc2
henriquemota <mota.henrique@gmail.com>
2025-10-27 09:36:21 -0300

    implementacao da leitura e gracao do arquivo

--- sources/async-storage/src/views/home.jsx
+++ sources/async-storage/src/views/home.jsx
@@ -3,11 +3,15 @@ import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 import { TextInput, Button } from 'react-native-paper'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import * as FileSystem from 'expo-file-system'
+import * as DocumentPicker from 'expo-document-picker'
 
 const Home = () => {
 	const { setItem, getItem } = AsyncStorage
 	const [loading, setLoading] = useState(false)
 	const [data, setData] = useState({ message: '', secret: '' })
+
+	const fileUri = FileSystem.documentDirectory + 'meuArquivo.txt'
 
 	const _gravar = async () => {
 		setLoading(true)
@@ -35,6 +39,30 @@ const Home = () => {
 		}
 	}
 
+	const _lerDiretorios = async () => {
+		try {
+			const { assets = null, canceled = false } = await DocumentPicker.getDocumentAsync({ copyToCacheDirectory: true })
+			if (!canceled) {
+				const conteudo = await FileSystem.readAsStringAsync(fileUri)
+				// const conteudo = await FileSystem.readAsStringAsync(assets[0].uri)
+				console.log(conteudo)
+			}
+			const conteudo = await FileSystem.readAsStringAsync(fileUri)
+			console.log(conteudo)
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
+	const _criarArquivo = async () => {
+		try {
+			await FileSystem.writeAsStringAsync(fileUri, 'Olá, mundo! Este é um teste.')
+			console.log('Arquivo criado com sucesso')
+		} catch (error) {
+			console.error('Erro ao criar arquivo:', error)
+		}
+	}
+
 	useEffect(() => {
 		_ler()
 	}, [])
@@ -52,6 +80,12 @@ const Home = () => {
 			</Button>
 			<Button mode='contained' onPress={_ler} loading={loading}>
 				Ler
+			</Button>
+			<Button mode='contained' onPress={_lerDiretorios} loading={loading}>
+				Abrir arquivos
+			</Button>
+			<Button mode='contained' onPress={_criarArquivo} loading={loading}>
+				Criar arquivo
 			</Button>
 			<StatusBar style='auto' />
 			<Text>{data.message}</Text>
